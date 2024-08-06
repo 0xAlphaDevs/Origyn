9d9f2d0ff347247fbfb875cf84a9310bebe06fb2
0xyshv <writetoyshv@gmail.com>
2024-08-06 10:48:17 +0530

    chore: add logo and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      ORIGYN
+      <Image src="/logo.png" width={150} height={150} alt="Logo" />
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-4xl font-bold text-primary">Welcome to ORIGYN !</h1>
+        <p className="text-muted-foreground">
+          A platform to sell digital goods using your World ID and creator provenance and rights using on-chain attestations.
+        </p>
+      </div>
     </main>
   );
 }
