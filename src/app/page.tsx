49be7c625f496a3a10d6032aabf0da0c16f59360
0xyshv <writetoyshv@gmail.com>
2024-08-07 11:09:55 +0530

    feat: add providers & wallet connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import {
   Card,
@@ -7,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { ConnectKitButton } from "connectkit";
 
 
 export default function Home() {
@@ -19,6 +22,7 @@ export default function Home() {
           A platform to sell digital goods using your World ID and creator provenance and rights using on-chain attestations.
         </p>
       </div>
+      <ConnectKitButton />
       <div className="grid grid-cols-3 gap-8 px-20 py-8 ">
         <Card className="shadow-sm border-none h-full w-full bg-violet-400 rounded-lg bg-opacity-15 border cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
           <CardHeader>
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client";
 
-const Providers = () => {
-  return (
-    <div>Providers</div>
-  )
+import * as React from "react";
+import { WagmiProvider, createConfig } from "wagmi";
+import { Chain } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+interface ProvidersProps {
+  children: React.ReactNode;
 }
 
-export default Providers
+const origynNetwork: Chain = {
+  id: 86420,
+  name: "Origyn Network",
+  rpcUrls: {
+    default: { http: ["https://rpc-origyn-network-ytbrtr4djq.t.conduit.xyz"] },
+    public: { http: ["https://rpc-origyn-network-ytbrtr4djq.t.conduit.xyz"] },
+  },
+  nativeCurrency: {
+    name: "ETH",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  blockExplorers: {
+    default: {
+      url: "https://explorer.origyn.alphadevs.dev",
+      name: "Origyn Explorer",
+    },
+  },
+};
+
+const config = createConfig(
+  getDefaultConfig({
+    walletConnectProjectId:
+      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+    chains: [origynNetwork],
+    appName: "Photon",
+  })
+);
+
+const queryClient = new QueryClient();
+
+export function Providers({ children, ...props }: ProvidersProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider theme="rounded">{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
