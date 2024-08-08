1128843e091fef41d60af5047058d1a7232f933d
0xyshv <writetoyshv@gmail.com>
2024-08-08 19:32:15 +0530

    chore: sync

--- src/app/(user)/marketplace/page.tsx
+++ src/app/(user)/marketplace/page.tsx
@@ -1,5 +1,6 @@
 import { Input } from '@/components/ui/input'
 import MarketplaceProductCard from '@/components/user/MarketplaceProductCard'
+import { SearchIcon } from 'lucide-react'
 import React from 'react'
 
 const Marketplace = () => {
@@ -7,7 +8,10 @@ const Marketplace = () => {
     <div className='py-8'>
       <div className="flex justify-between pb-12">
         <p className="text-3xl font-medium">Products</p>
-        <Input placeholder='Search products ...' className='w-72' />
+        <div className='relative'>
+          <SearchIcon className='text-muted-foreground h-6 w-4 absolute top-2 left-2' />
+          <Input placeholder='     Search products ...' className='w-72' />
+        </div>
       </div>
       <MarketplaceProductCard />
     </div>
