4932ca15cc7ca0e17548349ce959758c596cb907
0xyshv <writetoyshv@gmail.com>
2024-08-08 22:03:54 +0530

    feat: add PurchaseProductCard

--- src/app/(user)/purchases/page.tsx
+++ src/app/(user)/purchases/page.tsx
@@ -1,8 +1,20 @@
+import { Input } from '@/components/ui/input'
+import PurchaseProductCard from '@/components/user/PurchaseProductCard'
+import { SearchIcon } from 'lucide-react'
 import React from 'react'
 
 const MyPurchases = () => {
   return (
-    <div className='flex justify-center'>MyPurchases</div>
+    <div className='py-8'>
+      <div className='flex justify-between pb-10'>
+        <p className="text-3xl font-medium">My Purchases</p>
+        <div className='relative'>
+          <SearchIcon className='text-muted-foreground h-6 w-4 absolute top-2 left-2' />
+          <Input placeholder='     Search products ...' className='w-72' />
+        </div>
+      </div>
+      <PurchaseProductCard />
+    </div>
   )
 }
 
--- src/components/user/PurchaseProductCard.tsx
+++ src/components/user/PurchaseProductCard.tsx
@@ -16,14 +16,14 @@ import { Badge } from "../ui/badge";
 import { products } from "@/lib/dummyData";
 import Image from "next/image";
 
-interface MarketplaceProductCard {
+interface PurchaseProductCard {
 
 }
 
-const MarketplaceProductCard = ({ courseNftAddress }: any) => {
+const PurchaseProductCard = ({ courseNftAddress }: any) => {
   const router = useRouter();
   const { address } = useAccount();
-  const [course, setCourse] = React.useState<MarketplaceProductCard>({
+  const [course, setCourse] = React.useState<PurchaseProductCard>({
 
   });
 
@@ -53,8 +53,8 @@ const MarketplaceProductCard = ({ courseNftAddress }: any) => {
   //   }
   // }, [readContractsData, readContractsLoading]);
 
-  const handleViewProduct = (id: string) => {
-    router.push(`/marketplace/product/${id}`);
+  const handleDownload = () => {
+
   };
 
   // if (readContractsLoading) {
@@ -65,17 +65,17 @@ const MarketplaceProductCard = ({ courseNftAddress }: any) => {
   // }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+    <div className="grid grid-cols-1 md:grid-cols-1 gap-8 ">
       {products.map((product) => (
         <div key={product.id}
           className="group bg-card rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all"
         >
           <Image
-            src="/sample.jpg"
+            src="/sample2.jpg"
             width={400}
             height={400}
             alt="NFT Collection"
-            className="w-full h-56 object-cover group-hover:scale-105 transition-transform"
+            className="w-full h-32 object-cover"
             style={{ aspectRatio: "400/400", objectFit: "cover" }}
           />
           <div className="p-4 flex flex-col gap-2">
@@ -88,42 +88,15 @@ const MarketplaceProductCard = ({ courseNftAddress }: any) => {
             <p className="text-sm text-muted-foreground">
               {product.description}
             </p>
-            <Button onClick={() => handleViewProduct(product.id)}>
-              View Product
+            <Button onClick={() => handleDownload()}>
+              Download Files
             </Button>
           </div>
         </div>
-        // <div key={product.nftAddress} className="relative shadow-lg border">
-        //   <Image src={product.src} width={150} height={150} alt="Logo" className="opacity-75 w-full rounded-lg" />
-        //   <Card className="shadow-md absolute bottom-8 left-0 right-0 transform translate-y-1/2">
-        //     <CardHeader>
-        //       <CardTitle className="flex justify-between items-center">
-        //         <span>{product.name}</span>
-        //         <div className="flex gap-2 items-center">
-        //           <Badge>{product.id}</Badge>
-        //         </div>
-        //       </CardTitle>
-        //       <CardDescription>
-        //         {product.description}
-        //       </CardDescription>
-        //     </CardHeader>
-        //     <CardContent className="flex flex-col gap-2">
-        //       <p className="text-muted-foreground">
-        //         Price:{" "}
-        //         <strong className="text-lg font-bold">
-        //           {product.price}
-        //         </strong>
-        //       </p>
-        //       <Button onClick={() => handleViewProduct(product.id)}>
-        //         View Product
-        //       </Button>
-        //     </CardContent>
-        //   </Card>
-        // </div>
       ))}
     </div>
 
   );
 };
 
-export default MarketplaceProductCard;
+export default PurchaseProductCard;
