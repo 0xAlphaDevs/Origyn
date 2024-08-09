54c1d2163d2593b295317a1b6b1cc4d2776a412a
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-08-09 10:17:13 +0530

    chore: add types and prisma for data query

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,3 +8,11 @@ export interface NavbarProps {
   links: LinkItem[];
   supText: string;
 }
+
+export interface ProductAttestation {
+  productName: string;
+  productDescription: string;
+  productPrice: number;
+  creator: string;
+  contentURI: string;
+}
